0ca8e3efa300281ea5234528b3fc7a8233ccb812
Chab <francois.chabloz@gmail.com>
2026-07-05 13:19:31 +0200

    Email obfuscation

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,4 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
   if (yearEl) {
     yearEl.textContent = new Date().getFullYear();
   }
+
+  const supportEmailEl = document.getElementById("support-email");
+  if (supportEmailEl) {
+    const user = "contact";
+    const domain = "oxo-music";
+    const tld = "com";
+    const email = `${user}@${domain}.${tld}`;
+    const link = document.createElement("a");
+    link.className = "mail-link";
+    link.href = `mailto:${email}`;
+    link.textContent = email;
+    supportEmailEl.replaceWith(link);
+  }
 });
